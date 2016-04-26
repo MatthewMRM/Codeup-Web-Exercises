d5798b696ab87b0eae2c2dd009be2bac54446de2
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-26 08:58:10 -0500

    initial commit

---
+++ Public/my-favorite-things.php
@@ -0,0 +1,38 @@
+<?php 
+    $favoriteThings = ['Movies', 'Music', 'Traveling', 'Staying Active', 'Dancing'];  
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Favorite Things</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <!-- My CSS Stylesheet -->
+    <link rel="stylesheet" href="/css/my-favorite-things.css">
+</head>
+<body>
+    <div class="col s12 m4 l2"></div>
+    <div class="container col s12 m4 l8">
+        <h1>Favorite Things Assignment</h1>
+        <table class="highlight z-depth-5 col s12 m4 l8">
+            <tr>
+                <th class="flow-text">My Favorite Things</th>
+            </tr>
+                <?php foreach ($favoriteThings as $thing) { ?>
+                    <tr>
+                        <td class="flow-text"> 
+                         <?php echo $thing; ?>
+                    
+                        </td>
+                    </tr>
+                <?php }  ?>
+        </table>   
+    </div>
+    <div class="col s12 m4 l2"></div>
+ 
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>   
+</body>
+</html>
