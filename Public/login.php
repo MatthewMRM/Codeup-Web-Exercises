e7df24a141ada6a35f509d2fbb8cda488e80e72b
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-27 15:25:47 -0500

    login exercise - Post additions

--- Public/login.php
+++ Public/login.php
@@ -1,18 +1,41 @@
-<?php
-var_dump($_POST);
+<?php 
+
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//if (!empty($_POST)) {
+    //Check user password
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ('$username' === 'guest' && '$password' === 'password'){
+        header('Location: authorized.php');
+        die;
+    } else {
+
+        $message = 'this is a get request';
+}
+}
+
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>POST Example</title>
-</head>
-<body>
-    <form method="POST">
-        <label>Username</label>
-        <input type="text" name="name"><br>
-        <label>Password</label>
-        <input type="text" name="number"><br>
-        <input type="submit">
-    </form>
-</body>
-</html>
+
+ <!doctype html>
+ 
+ <html>
+     <head>
+         
+         <title>Login</title>
+
+     </head>
+     <body>
+        
+         <form method="POST">
+            <label>username</label>
+            <input type="text" name="username"><br>
+            <label>password</label>
+            <input type="password" name="password"><br>
+            <input type="submit" value= "Login">
+         </form>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     </body>
+ </html>
