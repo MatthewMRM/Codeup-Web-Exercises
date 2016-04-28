2d0fe62ffcbec9b1aba86d1b0cf7256f3bca3e82
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-28 14:59:09 -0500

    added inputhas and request

--- Public/login.php
+++ Public/login.php
@@ -1,49 +1,62 @@
 <?php
-session_start();
-$error = '';
-if (isset($_SESSION['logged_in_user'])){
-    header('location: authorized.php');
-}
-if (!isset($_POST['username']) && !isset($_POST['password'])){
-    $username = '';
-    $password = '';
-} else {
-    if ($_POST['username'] == 'guest' && $_POST['password'] == 'password'){
-        header('location: authorized.php');
-        $logged_in_user = $_POST['username'];
-        $_SESSION['logged_in_user'] = $logged_in_user;
-        exit();
-    } else {
-        $error = 'login failed';
-    }
-}
-?>
+// require once for EVERYTHING, no commas;
+require_once 'functions.php';
 
+    // starts session or finds a session
+    // allows us to use $_SESSION superglobal
+    session_start();
+    var_dump($_SESSION);
+    // variables for login check
+    $username = "guest";
+    $password = "password";
+    $message = "";
+    // checking $_POST superglobal for user input to check for login
+    $userInputUsername =  inputHas("username") ? inputGet("username") : '';
+    $userInputPassword =  inputHas("password") ? inputGet("password") : '';
+    // checking if 'LOGGED_IN_USER' key isset
+    // and if it has a value(not empty string)
+    // telling me that they are logged in and don't need to be here
+    if(isset($_SESSION['LOGGED_IN_USER']) && $_SESSION['LOGGED_IN_USER'] != "") {
+        // header redircts to another page
+        header('Location: /authorized.php');
+        // stops all other code from running
+        die();
+    // checking to see if both values match the correct login information
+    // doing this to login user if they entered the correct information
+    } else if ($userInputUsername == $username && $userInputPassword == $password) {
+        // create a session key
+        // save value as the username
+        $_SESSION['LOGGED_IN_USER'] = $username;
+        // header redircts to another page
+        header('Location: /authorized.php');
+        // stops all other code from running
+        die();
+    // checking to see if they entered the wrong information
+    // prevents message from showing when user submitted nothing or first time loading
+    } else if ($userInputUsername != '' || $userInputPassword != '') {
+        $message = "Incorrect login info";
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
+    <style>
+        
+    </style>
+
 </head>
 <body>
+    <p><?= $message; ?></p>
+    <form method="POST" action="">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" placeholder="Username" value="<?php escape('username'); ?>"></input>
+        <br>
 
-<form method="POST">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Password" value="<?php escape('password'); ?>"></input>
+        <br>
 
-    <label>Username</label>
-
-    <div><input type="text" name="username"></div>
-
-    <label>Password</label>
-
-    <div><input type="password" name="password"></div> <br>
-
-    <div><font color="red"><?= $error ?></font><div>
-
-    <input type="submit"> 
-
-</form>
-
-<a href="logout.php"> LOGOUT </a>
-
+        <button type="submit">Login</button>
+    </form>
 </body>
-
-</html>
