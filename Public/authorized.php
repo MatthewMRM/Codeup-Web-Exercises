b8485a473f7b5e5c58aee217ac268411efb5bf27
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-28 10:02:49 -0500

    added sesions

---
+++ Public/authorized.php
@@ -0,0 +1,14 @@
+<?php 
+ ?>
+ <!doctype html>
+ <html>
+     <head>
+         
+         <title>Authorized</title>
+
+     </head>
+     <body>
+         <h1>AUTHORIZED</h1>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     </body>
+ </html>
