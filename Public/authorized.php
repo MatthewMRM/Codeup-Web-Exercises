311e79228ac033bd005e393210411b5ca96f5674
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-28 10:27:43 -0500

    working on sessions

--- Public/authorized.php
+++ Public/authorized.php
@@ -1,14 +1,13 @@
-<?php 
- ?>
- <!doctype html>
- <html>
-     <head>
-         
-         <title>Authorized</title>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Authorized | PHP</title>
+</head>
+<body>
+  <h1>**** AUTHORIZED ****</h1>
+</body>
+</html>
 
-     </head>
-     <body>
-         <h1>AUTHORIZED</h1>
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-     </body>
- </html>
+
+
+ 
