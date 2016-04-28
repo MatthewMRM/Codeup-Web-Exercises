89850f5734290ce9336cca9c1a3fdb1bab1d17d8
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-28 15:01:22 -0500

    added some additional components

--- Public/logout.php
+++ Public/logout.php
@@ -1,9 +1,27 @@
 <?php
+// good
+// logout.php
 session_start();
 function clearSession(){
+    // clear $_SESSION array
     session_unset();
-    session_destroy();
-    header('location: login.php');
+    // delete session data on the server and send the client a new cookie
+    session_regenerate_id(true);
 }
 clearSession();
+header("Location: login.php");
+die();
 ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>logout.php</title>
+
+</head>
+<body>
+    <h1>logout.php</h1>
+
+
+</body>
+</html>
