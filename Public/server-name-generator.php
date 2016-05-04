fe49a4c2eb000d149e8955645dadadc60e386efb
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-04 14:03:33 -0500

    added functions

--- Public/server-name-generator.php
+++ Public/server-name-generator.php
@@ -1,16 +1,21 @@
 
 <?php
+//class code
+require_once 'GEN.php';
+//objects are state and behavior
 function pageController() {
-    $nouns = ['Boxer', 'Pitbull', 'Lab', 'Golden retriever', 'Poodle', 'Pug', 'Great Dane', 'shih Tzu', 'Chow Chow', 'Basset Hound'];
-    $adjectives = ['big', 'small', 'brown', 'gold', 'sad', 'gordo', 'tiny', 'long', 'angry', 'baked'];
+    $nouns = new NameGen();
+    $adejctives = new NameGen();
+    $nouns->setupGen('Boxer', 'Pitbull', 'Lab', 'Golden retriever', 'Poodle', 'Pug', 'Great Dane', 'shih Tzu', 'Chow Chow', 'Basset Hound');
+    $adjectives->setupGen('big', 'small', 'brown', 'gold', 'sad', 'gordo', 'tiny', 'long', 'angry', 'baked');
    
     function random ($array1) {
         $random_element = array_rand($array1);
         return $array1[$random_element];
     }
-    $data = array();
-    $data['adjectives'] = random($adjectives);
-    $data['nouns'] = random($nouns);
+    $this->data = array();
+    $this->data['adjectives'] = random($adjectives);
+    $this->data['nouns'] = random($nouns);
     return $data;
 }
 extract(pageController());
