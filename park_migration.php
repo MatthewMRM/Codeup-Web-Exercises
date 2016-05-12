7fa569d060261a790996ace4f9ada22cd2b1375b
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-12 14:14:01 -0500

    added required

--- park_migration.php
+++ park_migration.php
@@ -4,3 +4,5 @@ $dbc = new PDO('mysql:host=127.0.0.1;dbname=park_db.', 'parks_user', 'Panther661
 
 //Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+REQUIRE 'db_connect.php';
