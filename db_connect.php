6ac7e4a84d53684998b20d2fbccae7dc459a4c3d
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-12 09:34:59 -0500

    employee path

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,6 @@
 <?php
-$dbc = new PDO('mysql:host='.DB_HOSt.';dbname='.DB_NAME, DB_USER. DB_PASS);
+
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'vagrant', 'vagrant');
+//$dbc = new PDO('mysql:host='.DB_HOSt.';dbname='.DB_NAME, DB_USER. DB_PASS);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
