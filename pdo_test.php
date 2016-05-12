655b772b0d6b45754c0b98644c8fb3c10c917276
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-12 09:38:05 -0500

    itinal commit

---
+++ pdo_test.php
@@ -0,0 +1,11 @@
+<?php
+//define constants to sub into dbc
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'employees');
+define('DB_USER', 'vagrant');
+define('DB_PASS', 'vagrant');
+
+REQUIRE 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+?>
