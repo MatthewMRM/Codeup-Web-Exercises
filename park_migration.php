8678fdcde5a566d82e63db2a6ec98abc03189db8
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-12 14:10:02 -0500

    inital commit

---
+++ park_migration.php
@@ -0,0 +1,6 @@
+<?php
+//Get new instance of PDO object
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=park_db.', 'parks_user', 'Panther6612$');
+
+//Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
