1e8b60ffaf4e40af9c4136cf71f13618fb0a6604
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-13 09:12:25 -0500

    removed and added table

--- park_migration.php
+++ park_migration.php
@@ -6,3 +6,18 @@ $dbc = new PDO('mysql:host=127.0.0.1;dbname=park_db.', 'parks_user', 'Panther661
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 REQUIRE 'db_connect.php';
+
+$query = 'DROP TABLE IF EXISTS national_parks';
+//run the above query
+$dbc->exec($query);
+$query = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100),
+    date_established DATE,
+    area_in_acres DOUBLE,
+    PRIMARY KEY (id)
+)';
+//run the above query
+$dbc->exec($query);
+?>
