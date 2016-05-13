25dfe9abbee8ecca6d266145501362a3be7484e9
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-13 14:32:19 -0500

    park seeder

--- park_seeder.php
+++ park_seeder.php
@@ -1,6 +1,6 @@
 <?php
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=parks_db.', 'vagrant', 'vagrant');
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=parks_db.', 'codeup', 'Panther6612$');
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //add query to delete all records from db
