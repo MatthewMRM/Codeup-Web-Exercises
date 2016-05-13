d5fa150ab8bb026f0a09f3119b3e8dcda22f74b8
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-13 11:06:35 -0500

    Started over and switched direction

--- Public/national_parks.php
+++ Public/national_parks.php
@@ -1,8 +1,5 @@
+<!-- national_parks.php -->
 <?php
-$errors = [];
-$parkTextField = 'matthew';
-
-// var_dump($check);
 // On each page load, it should retrieve all records from the database and display them.
 // Modify your query to load only four parks at a time.
 // Use a parameter in $_GET to determine which page the user is on and load only the appropriate parks for that page.
@@ -13,16 +10,33 @@ define('DB_NAME', 'parks_db');
 define('DB_USER', 'vagrant');
 define('DB_PASS', 'vagrant');
 require_once '../db_connect.php';
-// input is how we get our page numbers
-require_once '../Input.php';
-// this will give me OFFSET
-
-$limit = 4;
-$page = Input::get('page',1);
-$offset = ($page * $limit) - $limit;
-// inside of this string is only SQL
-// double quotes b/c it is dynamic!!!
-$stmt=$dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset");
+$stmt=$dbc->query('SELECT * FROM national_parks');
 $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // var_dump($parks);
 ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>national_parks.php</title>
+
+</head>
+<body>
+    <h1> test </h1>
+    <?php foreach($parks as $park): ?>
+        <ul>
+            <h3> <?= $park['name'] . PHP_EOL  ?></h3>
+                <li><?= $park['location'] . PHP_EOL?> </li>
+                <li><?= $park['date_established'] . PHP_EOL  ?> </li>
+                <li><?= $park['area_in_acres'] . PHP_EOL ?> </li>
+                    <?= PHP_EOL . "\n" ?>
+        </ul>
+    <?php endforeach; ?>
+
+</body>
+</html>
+
+
+<ul>
+    <li> </li>
+</ul>
