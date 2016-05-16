779caf4d28b3340c0fa9fcfd74c68d302427d4f5
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-16 10:02:55 -0500

    test

--- Public/national_parks.php
+++ Public/national_parks.php
@@ -1,4 +1,4 @@
-<!-- national_parks.php -->
+
 <?php
 // On each page load, it should retrieve all records from the database and display them.
 // Modify your query to load only four parks at a time.
@@ -7,36 +7,67 @@
 // Add some logic to determine whether or not to show the next and/or previous page links.
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'parks_db.');
-define('DB_USER', 'parks_user');
+define('DB_USER', 'vagrant');
 define('DB_PASS', 'vagrant');
 require_once '../db_connect.php';
-$stmt=$dbc->query('SELECT * FROM national_parks');
-$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// var_dump($parks);
+REQUIRE '../Input.php';
+function pageController($dbc) {
+    $data = [];
+    $data['page'] = Input::has('page') ? Input::get('page') : 1;
+    $offset = ($data['page'] - 1) * 4; 
+    $stmt = $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset);
+    
+    $data['parks'] = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    return $data;
+}
+    extract(pageController($dbc));
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html>
 <head>
+
     <meta charset="utf-8">
-    <title>national_parks.php</title>
+    <title>National Parks</title>
+
+    <!-- font -->
+    
 
 </head>
 <body>
-    <h1> test </h1>
-    <?php foreach($parks as $park): ?>
-        <ul>
-            <h3> <?= $park['name'] . PHP_EOL  ?></h3>
-                <li><?= $park['location'] . PHP_EOL?> </li>
-                <li><?= $park['date_established'] . PHP_EOL  ?> </li>
-                <li><?= $park['area_in_acres'] . PHP_EOL ?> </li>
-                    <?= PHP_EOL . "\n" ?>
-        </ul>
-    <?php endforeach; ?>
+
+<div>
+    <p>You are on page <?=$page;?> </p>
+    <h2>National Parks of the United States</h2>
+    <div>
+        <table class="shadow">
+            <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Date Established</th>
+                <th>Acreage</th>
+            </tr>
+
+            <?php foreach($parks as $park) : ?>
+                <tr><td><?= $park['name']; ?></td>
+                <td><?= $park['location']; ?></td>
+                <td><?= date_format(date_create($park['date_established']), 'm-d-Y' ); ?></td>
+                <td><?= $park['area_in_acres']; ?></td></tr>
+            <?php endforeach; ?>
+
+        </table>
+    </div>
+</div>
+
+<footer class="footer"> 
+    <?php if($page>=2): ?> 
+        <a href="?page=<?= $page - 1 ?>" class="btn btn-default">Previous</a>
+    <?php endif; ?>
+
+    <?php if($page < count($parks) ): ?>
+        <a href="?page=<?= $page + 1 ?>" class="btn btn-default">Next</a>
+    <?php endif; ?>
+</footer>
 
 </body>
 </html>
-
-
-<ul>
-    <li> </li>
-</ul>
