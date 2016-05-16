b907ee225c98cc1c06ae414f23f4c88beed93275
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-16 10:02:17 -0500

    test

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,8 @@
+
 <?php
-
-//$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'vagrant', 'vagrant');
-
+REQUIRE 'db_credentials.php';
 // Get new instance of PDO object
-$dbc = new PDO(
-    'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
-    DB_USER,
-    DB_PASS);
-
-
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
