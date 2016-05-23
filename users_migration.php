b55d9043fc56f53b37b4d2bd4a82e2e085108cdc
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-23 14:38:49 -0500

    commit for user migration

--- users_migration.php
+++ users_migration.php
@@ -1,1 +1,19 @@
+
 <?php
+REQUIRE 'adlister_credentials.php';
+REQUIRE 'db_connect.php';
+$query = 'DROP TABLE IF EXISTS national_parks';
+//run the above query
+$dbc->exec($query);
+$query = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100),
+    date_established DATE,
+    area_in_acres DOUBLE,
+    description TEXT(500),
+    PRIMARY KEY (id)
+)';
+//run the above query
+$dbc->exec($query);
+?>
